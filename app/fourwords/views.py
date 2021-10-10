681107edd3daf034e174effdf1095259526af94e
root <root@localhost>
2021-10-10 20:58:27 +0000

    certs

--- app/fourwords/views.py
+++ app/fourwords/views.py
@@ -7,7 +7,6 @@ import random
 with open('/opt/app/fourwords/rockyou-50.txt') as f:
     lines = f.read().splitlines()
 
-
 def index(request):
     flag='FLAG{'+'-'.join([ lines[random.randint(0,len(lines))] for i in range(4)])+'}'
     return HttpResponse(flag)
